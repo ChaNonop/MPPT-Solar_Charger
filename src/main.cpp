6f8c116049c4be4e84d56c42b5865da491d909f4
Chanon <sabeet57@gmail.com>
2026-02-08 11:22:49 +0700

    Clean up commented-out MQTT and button logic code
    Removed commented-out code related to MQTT and button handling.

--- src/main.cpp
+++ src/main.cpp
@@ -54,38 +54,6 @@ void loop(){
   }
 
 
-// const char* ssid = "Somwang";
-// const char* password = "03143681"; 
-// const char* mqtt_server = "broker.hivemq.com"; 
-// const int mqtt_port = 1883;
-
-// // Client ID ควรจะไม่ซ้ำใคร
-// const char* mqtt_Client = "ESP8266_My"; 
-// const char* mqtt_username = "";  
-// const char* mqtt_password = ""; 
-
-// WiFiClient espClient; 
-// PubSubClient client(espClient);
-
-
-// void reconnect() {
-//   while (!client.connected()) {
-//     Serial.print("Attempting MQTT connection...");
-//     if (client.connect(mqtt_Client, mqtt_username, mqtt_password)) { 
-//       Serial.println("connected");
-      
-//       client.subscribe("/Command"); 
-//       Serial.println("Subscribed to /Command");
-//     }
-//     else {
-//       Serial.print("failed, rc=");
-//       Serial.print(client.state());
-//       Serial.println(" try again in 5 seconds");
-//       delay(5000);
-//     }
-//   }
-// }
-
 // void callback(char* topic, byte* payload, unsigned int length) {
 //   Serial.print("Message arrived [");
 //   Serial.print(topic);
@@ -109,34 +77,4 @@ void loop(){
 //       Serial.println("LED OFF"); 
 //     }
 //   } 
-// }
-
-// void loop() {
-//   int currentButtonState = digitalRead(Pin_button);
-
-//   // Logic ป้องกันการส่งรัวๆ
-//   // ตรวจสอบว่าค่าปุ่มเปลี่ยนไปจากรอบที่แล้วหรือไม่
-//   if (currentButtonState != lastButtonState) {
-    
-//     // ถ้าสถานะปัจจุบันคือ LOW (แปลว่าเพิ่งถูกกด)
-//     if (currentButtonState == LOW) {
-//       // 1. สั่ง LED Built-in ติด (Active LOW)
-//       digitalWrite(LED_BUILTIN, LOW); 
-      
-//       // 2. ส่ง Publish ค่า 1 ไปที่ /non
-//       Serial.println("Button Pressed -> Publishing '1'");
-//       client.publish("/non","1"); 
-//     } 
-//     // ถ้าสถานะปัจจุบันคือ HIGH (แปลว่าเพิ่งปล่อยมือ)
-//     else {
-//       // สั่ง LED Built-in ดับ
-//       digitalWrite(LED_BUILTIN, HIGH); 
-//     }
-    
-//     // หน่วงเวลาเล็กน้อยแก้สั่น (Debounce)
-//     delay(50); 
-//   }
-  
-//   // จำค่าสถานะไว้เทียบในรอบถัดไป
-//   lastButtonState = currentButtonState;
 // }
